cd2a1f5edae80b64a744cc3c3a3c33d60b5a0d92
steve-alex <steve.alex@hotmail.co.uk>
2019-11-23 16:56:10 +0000

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,20 +1,64 @@
-import React, { Component } from 'react';
+import React, {useState, useEffect} from 'react';
 import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
 
-// Endpoint!
-const API = "http://localhost:3000/sushis"
+const BASE_URL = 'http://localhost:3000'
+const SUSHI_URL = `${BASE_URL}/sushis`
 
-class App extends Component {
+const App = () => {
+  const [sushis, setSushis] = useState([])
+  const [money, setMoney] = useState(100)
+  const [index, setIndex] = useState(0)
+  const [eatenSushi, setEatenSushi] = useState([])
 
-  render() {
-    return (
-      <div className="app">
-        <SushiContainer  />
-        <Table />
-      </div>
-    );
+  useEffect(() => {
+    fetchSushi();
+  }, [])
+
+  const getSushis = () => {
+    return sushis.slice(index, index + 4)
   }
+
+  const moreSushi = (event) => {
+    event.preventDefault()
+    setIndex(index + 4)
+  }
+
+  const fetchSushi = async() => {
+    return fetch(SUSHI_URL)
+      .then(resp => resp.json())
+      .then(sushiData => setSushis(sushiData))
+  }
+  
+  const eatSushi = (sushi) => {
+    if (sufficientFunds(sushi)) {
+      setEatenSushi([...eatenSushi, sushi])
+      setMoney(money - sushi.price)
+    } else {
+      window.alert("YOU BROKE MOFO")
+    }
+  }
+
+  const sufficientFunds = (sushi) => {
+    if (sushi.price <= money) {
+      return true
+    }
+    return false
+  }
+
+  return (
+    <div className="app">
+      <SushiContainer
+        sushis={getSushis()}
+        eatSushi={eatSushi}
+        moreSushi={moreSushi}/>
+      <Table
+        money={money}
+        setMoney={setMoney}
+        eatenSushi={eatenSushi}/>
+    </div>
+  )
+
 }
 
 export default App;
