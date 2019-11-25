08a67fbfa932f72df0d470f188b11e97ad5e0e72
github username <github email address>
2019-11-25 01:30:14 +0000

    Done.

--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,18 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
+  
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
+    
+        { props.fourSushis().map((sushi)=>{
+          return <Sushi sushi = {sushi} id= {sushi.id} dropSushi= {props.dropSushi}/>
+         })}
+      
+        <MoreButton plus = {props.plus}/>
       </div>
     </Fragment>
   )
